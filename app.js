9ea888d5ed7db791d13dfcb8696ff8c51d6d8c31
edwardtimsack <edwardtimothysackie@gmail.com>
2023-04-12 23:55:10 +0000

    almost done

--- app.js
+++ app.js
@@ -24,51 +24,61 @@ startBtn.addEventListener("click", function () {
       for (let i = 0; i < numbers.length; i++) {
         const button = document.createElement("button");
         button.textContent = numbers[i];
+       
         button.addEventListener("click", function (){
+
+          if (lives.textContent <= 0) {
+            console.log("Here");
+            words.style.display = 'block';
+            words.textContent = "Sorry, Game Over!!";
+            
+            
+            button.forEach(button => {
+              button.disabled = true;
+              console.log("btn", button)
+              button.style.backgroundColor = 'red';
+            });
+           
+          }
 
           if (Number(button.textContent) === luckyNumber) { // check if the button value is equal to the lucky number
             words.textContent = "Congratulations, You Won!"; // log a message to the console if the button value is the lucky number
             button.disabled = true;
-            button.style.backgroundColor = 'green';
+            button.style.backgroundColor = 'white';
             gameOver = true;
 
 
-            const buttons = document.querySelectorAll("button");
+            let buttons = document.querySelectorAll("button");
             for (let j = 0; j < buttons.length; j++) {
               buttons[j].disabled = true;
-              button.style.backgroundColor = 'red';
+              button.style.backgroundColor = 'green';
 
             }
             words.textContent = "Congratulations, You Won!"
 
           } else if(Number(button.textContent) < luckyNumber) {
+            lives -1;
             words.style.display = 'block';
             words.textContent = "Guess Too Low!";
             button.disabled = true;
             button.style.backgroundColor = 'gold';
+            button.style.color = 'black'
             lives.textContent = Number(lives.textContent) - 1;
             
 
           } else if(Number(button.textContent) !== luckyNumber){
             words.style.display = 'block';
-            words.textContent = "Guess too High";
+            words.textContent = "Guess Too High";
             button.disabled = true;
             button.style.backgroundColor = 'red';
+            button.style.color = 'black'
             lives.textContent = Number(lives.textContent) - 1;
 
           }
           console.log(button.textContent); //log the value to the console when clicked
         });
 
-        if (Number(button.textContent) === 0) {
-          words.style.display = 'block';
-          words.textContent = "Sorry, Game Over!!";
-          button.forEach(button => {
-            button.disabled = true;
-            button.style.backgroundColor = 'red';
-          });
-         
-        }
+        
         
         container.appendChild(button);
       }
@@ -79,18 +89,6 @@ startBtn.addEventListener("click", function () {
 });
 
 
-// if (live_num == 0) {
-//   words.style.display = 'block';
-//   words.textContent = "Sorry, Game over!";
-//   const buttons = document.querySelectorAll('.btn button');
-//   buttons.forEach(button => {
-//     button.disabled = true;
-//     button.style.backgroundColor = 'red';
-//   });
-// }
-// if (Number(button.textContent) !== luckyNumber) {
-//   lives.textContent = Number(lives.textContent) - 1;
-// }
 
 
 
